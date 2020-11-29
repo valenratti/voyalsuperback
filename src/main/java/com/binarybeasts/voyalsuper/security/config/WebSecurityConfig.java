f3735e31d28634ee5c66e2891675d6612524da0f
Valentin Segundo Ratti <vratti@itba.edu.ar>
2020-11-29 09:01:35 -0300

    fixes.

--- src/main/java/com/binarybeasts/voyalsuper/security/config/WebSecurityConfig.java
+++ src/main/java/com/binarybeasts/voyalsuper/security/config/WebSecurityConfig.java
@@ -1,53 +1,59 @@
 package com.binarybeasts.voyalsuper.security.config;
 
 import com.binarybeasts.voyalsuper.security.jwt.JwtAuthEntryPoint;
-import com.binarybeasts.voyalsuper.security.jwt.JwtAuthTokenFilter;
+import com.binarybeasts.voyalsuper.security.jwt.JwtRequestFilter;
 import com.binarybeasts.voyalsuper.service.JwtUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
-@ComponentScan(basePackages = { "com.baeldung.security" })
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtUserDetailsService userDetailsService;
 
-    private final JwtAuthEntryPoint unauthorizedHandler;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
 
     @Autowired
-    public WebSecurityConfig( JwtUserDetailsService userDetailsService, JwtAuthEntryPoint unauthorizedHandler) {
+    public WebSecurityConfig(JwtUserDetailsService userDetailsService, JwtAuthEntryPoint jwtAuthEntryPoint) {
         this.userDetailsService = userDetailsService;
-        this.unauthorizedHandler = unauthorizedHandler;
+        this.jwtAuthEntryPoint = jwtAuthEntryPoint;
     }
 
-    @Bean
-    public JwtAuthTokenFilter authenticationJwtTokenFilter() {
-        return new JwtAuthTokenFilter();
-    }
 
     @Override
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and().exceptionHandling().authenticationEntryPoint(jwtAuthEntryPoint).
+                and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public JwtRequestFilter authenticationJwtTokenFilter() {
+        return new JwtRequestFilter();
     }
 
     /*@Override
@@ -87,20 +93,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
 
 }
